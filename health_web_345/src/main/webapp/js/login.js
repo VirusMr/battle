307d837d0171f2c7bf9d00255f87bd6f88e6f9e2
leexm <858631555@qq.com>
2019-06-25 12:13:05 +0800

    小细节优化

---
+++ health_web_345/src/main/webapp/js/login.js
@@ -0,0 +1,204 @@
+/*$(function(){
+	
+//	$('.header').after("test")
+	
+	
+	$('#u').blur(function(){
+		var username = $('#u').val();
+		if(username == null || username == ""){
+			$('#tip').html("<font color = 'red ' size = '4'>   请输入账号！！</font>");
+		}
+	})
+	
+	$('#p').blur(function(){
+		var pass = $('#u').val();
+		if(pass == null || pass == ""){
+			$('#tip').html("<font color = 'red ' size = '4'>   请输入密码！！</font>");
+		}
+	})
+	
+	
+	
+	
+})*/
+
+$(function(){
+	//按钮点击事件  此处使用ajax方式提交   需将页面上的提交按钮 type属性修改为'button' form的表单提交路径删除
+	$('#sub').click(function(){
+		//非空验证 为空则停止方法 并将提示显示到页面上
+		var username = $('#u').val();
+		var pass = $('#p').val();
+		if(username == null || username == ""){
+			$('#tip').html("<font color = 'red ' size = '4'>   请输入账号！！</font>");
+			return;
+		}
+		if(pass == null || pass == ""){
+			$('#tip').html("<font color = 'red ' size = '4'>   请输入密码！！</font>");
+			return;
+		}
+		
+		//获取表单数据
+		var formdata = {'username':username,'password':pass};
+		
+		 //ajax提交数据
+		$.ajax({
+			url:'loginCheck',
+			data:formdata,
+			type:'post',
+			success:function(data){
+				//返回200则请求主页  其他返回值提示账号秘密错误
+				if(data == '200'){
+					window.location.href='index';
+				}else{
+					$('#tip').html("<font color = 'red ' size = '4'>账号或密码错误！！</font>");
+				}
+			}
+			
+		})
+		
+		
+		
+		
+	})
+})
+
+$(function(){
+	
+	$('#switch_qlogin').click(function(){
+		$('#switch_login').removeClass("switch_btn_focus").addClass('switch_btn');
+		$('#switch_qlogin').removeClass("switch_btn").addClass('switch_btn_focus');
+		$('#switch_bottom').animate({left:'0px',width:'70px'});
+		$('#qlogin').css('display','none');
+		$('#web_qr_login').css('display','block');
+		
+		});
+	$('#switch_login').click(function(){
+		
+		$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
+		$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
+		$('#switch_bottom').animate({left:'154px',width:'70px'});
+		
+		$('#qlogin').css('display','block');
+		$('#web_qr_login').css('display','none');
+		});
+if(getParam("a")=='0')
+{
+	$('#switch_login').trigger('click');
+}
+
+	});
+	
+function logintab(){
+	scrollTo(0);
+	$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
+	$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
+	$('#switch_bottom').animate({left:'154px',width:'96px'});
+	$('#qlogin').css('display','none');
+	$('#web_qr_login').css('display','block');
+	
+}
+
+
+//根据参数名获得该参数 pname等于想要的参数名 
+function getParam(pname) { 
+    var params = location.search.substr(1); // 获取参数 平且去掉？ 
+    var ArrParam = params.split('&'); 
+    if (ArrParam.length == 1) { 
+        //只有一个参数的情况 
+        return params.split('=')[1]; 
+    } 
+    else { 
+         //多个参数参数的情况 
+        for (var i = 0; i < ArrParam.length; i++) { 
+            if (ArrParam[i].split('=')[0] == pname) { 
+                return ArrParam[i].split('=')[1]; 
+            } 
+        } 
+    } 
+}  
+
+
+var reMethod = "GET",
+	pwdmin = 6;
+
+$(document).ready(function() {
+
+
+	$('#reg').click(function() {
+
+		if ($('#user').val() == "") {
+			$('#user').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#userCue').html("<font color='red'><b>×用户名不能为空</b></font>");
+			return false;
+		}
+
+
+
+		if ($('#user').val().length < 4 || $('#user').val().length > 16) {
+
+			$('#user').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#userCue').html("<font color='red'><b>×用户名位4-16字符</b></font>");
+			return false;
+
+		}
+		$.ajax({
+			type: reMethod,
+			url: "/member/ajaxyz.php",
+			data: "uid=" + $("#user").val() + '&temp=' + new Date(),
+			dataType: 'html',
+			success: function(result) {
+
+				if (result.length > 2) {
+					$('#user').focus().css({
+						border: "1px solid red",
+						boxShadow: "0 0 2px red"
+					});$("#userCue").html(result);
+					return false;
+				} else {
+					$('#user').css({
+						border: "1px solid #D7D7D7",
+						boxShadow: "none"
+					});
+				}
+
+			}
+		});
+
+
+		if ($('#passwd').val().length < pwdmin) {
+			$('#passwd').focus();
+			$('#userCue').html("<font color='red'><b>×密码不能小于" + pwdmin + "位</b></font>");
+			return false;
+		}
+		if ($('#passwd2').val() != $('#passwd').val()) {
+			$('#passwd2').focus();
+			$('#userCue').html("<font color='red'><b>×两次密码不一致！</b></font>");
+			return false;
+		}
+
+		var sqq = /^[1-9]{1}[0-9]{4,9}$/;
+		if (!sqq.test($('#qq').val()) || $('#qq').val().length < 5 || $('#qq').val().length > 12) {
+			$('#qq').focus().css({
+				border: "1px solid red",
+				boxShadow: "0 0 2px red"
+			});
+			$('#userCue').html("<font color='red'><b>×QQ号码格式不正确</b></font>");return false;
+		} else {
+			$('#qq').css({
+				border: "1px solid #D7D7D7",
+				boxShadow: "none"
+			});
+			
+		}
+
+		$('#regUser').submit();
+	});
+	
+
+});
